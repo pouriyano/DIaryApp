f20e8d22e34467a4d9c580e3f4bc0678ff2f863f
pouriya noghani_mahdi shariatikia <pouriyan.db@gmail.com>
2025-02-01 02:40:47 -0800

    initial

--- app/src/main/java/com/example/Diary/DiaryViewModel.kt
+++ app/src/main/java/com/example/Diary/DiaryViewModel.kt
@@ -4,35 +4,33 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class DiaryViewModel(val diaryRepository: DiaryRepository=Graph.diaryRepository) :ViewModel()
 {
+    private val _getAllDiaries = MutableStateFlow<List<Diary>>(emptyList())
+    val getAllDiaries: StateFlow<List<Diary>> get() = _getAllDiaries
 
-
-
-
-
-        lateinit var getAllDiaries: Flow<List<Diary>>
     init {
         viewModelScope.launch {
-            getAllDiaries = diaryRepository.getDiaries()
+            diaryRepository.getDiaries().collect {
+                _getAllDiaries.value = it
+            }
         }
     }
+
     fun addDiary(diary: Diary) {
         viewModelScope.launch(Dispatchers.IO) {
             diaryRepository.addDiary(diary)
         }
     }
+
     fun updateDiary(diary: Diary) {
-
-
-            viewModelScope.launch(Dispatchers.IO) {
-                diaryRepository.UpdateADiary(diary)
-            }
-
-
-
+        viewModelScope.launch(Dispatchers.IO) {
+            diaryRepository.UpdateADiary(diary)
+        }
     }
 
     fun deleteDiary(diary: Diary) {
